7ca63ea61fffbb1e33da7f9acb9d17a9f9fad5e6
yer2018 <42699536+yer2018@users.noreply.github.com>
2022-03-24 18:46:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -52,8 +52,7 @@ echo "exit 0" >> package/lean/default-settings/files/zzz-default-settings
 # Modify default PassWord
 sed -i 's/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/root:$1$a87b3JDA$O5S5vtQFGIL9deGI2KeBg1:0:0:99999:7:::/g' package/lean/default-settings/files/zzz-default-settings
 # Add ssh-rsa
-sed -i '40a echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClNPo83GB5AiEmDTvY4gQEuTHVQ5qqDyRIa8RIus6D/UL5CNWx6+0JO2Vtigsxiq5Y8JyoLBW0Cs2oTWGLOQmGOf6S2suRzTv+UZotvCzFqnWHa6uwdQnEuYPLhR4jQs1rr+reBIHX8fZPda5KUBzfyFwHqANMfCLi3+KC3SY+BxcqmWY0d73oXriKUaKsBUC0cO58k5MbUuXQUdhd4K+MbEkJesO5vlOxQ0GA3JGiGYiZhv6M3f6cRDKTpralcFAbuvjwuk7+wM5hWTO2pFxk6He+W1bY7qrn2QNvIPwQv95aQQp/NekbGscJHSJrj5vTIewkOwdTDjUkeEoRevsV9LlJfQmfHcAlgdDRFQ+SUdjbanrKlq8DNMqYqw8si0EiIbIoftn/2ST9shV/CWImb/SV7zUk2fKcvPUfP6OId3KmG7eaVRB9g3O2sF13PvUQuyaiX+nvZtWBoxBMtbZ58P+2RVM7iYLI2llBTWtdXSzen5LoqS4rLP65x+j2VZc= ye@YedeMacBook-Pro.local
-" > /etc/dropbear/authorized_keys' package/lean/default-settings/files/zzz-default-settings
+sed -i '40a echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQClNPo83GB5AiEmDTvY4gQEuTHVQ5qqDyRIa8RIus6D/UL5CNWx6+0JO2Vtigsxiq5Y8JyoLBW0Cs2oTWGLOQmGOf6S2suRzTv+UZotvCzFqnWHa6uwdQnEuYPLhR4jQs1rr+reBIHX8fZPda5KUBzfyFwHqANMfCLi3+KC3SY+BxcqmWY0d73oXriKUaKsBUC0cO58k5MbUuXQUdhd4K+MbEkJesO5vlOxQ0GA3JGiGYiZhv6M3f6cRDKTpralcFAbuvjwuk7+wM5hWTO2pFxk6He+W1bY7qrn2QNvIPwQv95aQQp/NekbGscJHSJrj5vTIewkOwdTDjUkeEoRevsV9LlJfQmfHcAlgdDRFQ+SUdjbanrKlq8DNMqYqw8si0EiIbIoftn/2ST9shV/CWImb/SV7zUk2fKcvPUfP6OId3KmG7eaVRB9g3O2sF13PvUQuyaiX+nvZtWBoxBMtbZ58P+2RVM7iYLI2llBTWtdXSzen5LoqS4rLP65x+j2VZc= ye@YedeMacBook-Pro.local" > /etc/dropbear/authorized_keys' package/lean/default-settings/files/zzz-default-settings
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile

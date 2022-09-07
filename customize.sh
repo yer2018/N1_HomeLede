f81ec5dc3e95e20f3075ca6a256de9895cff7612
yer2018 <13868869731@139.com>
2022-09-07 11:48:17 +0800

    9-7

--- customize.sh
+++ customize.sh
@@ -57,7 +57,7 @@ sed -i '40a echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC5mxNSzSRs61tdDIp2GfqQRc
 
 # Modify default theme（FROM uci-theme-bootstrap CHANGE TO luci-theme-material）
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' ./feeds/luci/collections/luci/Makefile
-\cp -rf ../bg1.jpg feeds/xiaoqingfeng/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+#\cp -rf ../bg1.jpg feeds/xiaoqingfeng/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 sed -i 's/width: 420px;/width: 330px;/g' feeds/xiaoqingfeng/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 sed -i 's/margin-left: 5%;/margin-left: 0%;/g' feeds/xiaoqingfeng/luci-theme-argon/htdocs/luci-static/argon/css/cascade.css
 
